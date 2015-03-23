7b9d9b92b29218bb2b15d3ef131d4eb6cb2df60f
Ben Davis <bendavis78@gmail.com>
2015-03-23 16:02:40 -0500

    Refactored for year-by-year selection instead of autopopulating

---
+++ paper-calendar.js
@@ -0,0 +1,129 @@
+(function() {
+  Polymer("paper-calendar", {
+    once: function(obj, eventName, callback) {
+      var doCallback = function() {
+        callback.apply(obj, arguments);
+        obj.removeEventListener(eventName, doCallback);
+      };
+      obj.addEventListener(eventName, doCallback);
+    },
+    publish: {
+      locale: null,
+      minYear: null,
+      maxYear: null,
+      initialMonth: null,
+      isTouch: {type: 'boolean', value: false, reflect: true}
+    },
+    localeChanged: function() {
+      moment.locale(this.locale);
+    },
+    dateClicked: function(e) {
+      var element = e.originalTarget ? e.originalTarget : e.toElement;
+    },
+    ready: function() {
+      this.selectedDate = null;
+      this.isTouch = 'ontouchstart' in window;
+      this.months = [];
+      this.years = [];
+      this.today = new Date();
+      this._physicalYears = [];
+      this.initialDate = this.initialDate ? this.initialDate : this.today;
+      this.today.setHours(0, 0, 0, 0);
+      this.weekdays = moment.weekdays();
+      this.monthNames = moment.months();
+      this.localeChanged();
+      this.startYear = this.startYear ? this.startYear : 1900;
+      this.endYear = this.endYear ? this.endYear : 2100;
+      for (var y=this.startYear; y<=this.endYear; y++) {
+        this.years.push(y);
+      }
+      this.async(function() {
+        this.populate(this.initialDate.getFullYear());
+      });
+      this.scrollToDate(this.initialDate);
+    },
+    /* TODO: figure out a user-friendly way to auto-populate years
+     * It takes more time on mobile, so there should be some indication that a
+     * year is loading. One idea is to make it so that when the user scrolls to
+     * the bottom, an indicator appears showing that the content for that year is
+     * loading. We'd need extra logic to handle scrolling and removal of years
+     * that are hidden from view.
+    loadMoreUpper: function() {
+    },
+    loadMoreLower: function() {
+    }
+    */
+    populate: function(year) {
+      // this currently replaces the 
+      this.months = [];
+      var month, days, day, date = new Date();
+      var thisYear = this.today.getFullYear();
+      var thisMonth = this.today.getMonth();
+      var thisDay = this.today.getDate();
+
+      date.setYear(year);
+      for (month=0; month<12; month++) {
+        // days are split into weeks
+        days = [[]];
+        day = 1;
+        date.setMonth(month);
+        date.setDate(1);
+        
+        // add "padding" days
+        for (d=0; d<date.getDay(); d++) {
+          days[0].push({day: null});
+        }
+
+        // add actual days 
+        while (date.getMonth() == month) {
+          if (d % 7 == 0) {
+            // start new week
+            days.push([]);
+          }
+          days[days.length-1].push({
+            name: year + '-' + (month+1) + '-' + day,
+            year: year,
+            month: month,
+            day: day,
+            isToday: year == thisYear && month == thisMonth && day == thisDay
+          });
+          date.setDate(++day);
+          d++;
+        }
+        monthData = {
+          year: year,
+          month: month,
+          days: days,
+          domReady: false
+        };
+        this.months.push(monthData);
+      }
+    },
+    getMonthIdx: function(year, month) {
+      var yearDiff = (year - this.months[0].year);
+      var monthDiff = (month - this.months[0].month);
+      return (yearDiff * 12) + monthDiff;
+    },
+    scrollToMonth: function(year, month) {
+      this.async(function() {
+        var el = this.querySelector(':host /deep/ .month-' + year + '-' + month);
+        this.$.calendarList.scrollTop = el.offsetTop;
+      });
+    },
+    scrollToDate: function(date) {
+      if (!date) {
+        date = this.initialDate;
+      }
+      this.scrollToMonth(date.getFullYear(), date.getMonth());
+    },
+    dateSelected: function(event, detail) {
+      var dateStr = detail.item.getAttribute('label');
+      this.selectedDate = new Date(dateStr);
+      this.fire('date-select', {date: this.selectedDate}, this);
+      this.scrollToDate(this.selectedDate);
+    },
+    _getDayName: function(date) {
+      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    },
+  });
+})();
--- paper-date-picker.js
+++ paper-date-picker.js
@@ -1,207 +1,88 @@
-Polymer("paper-date-picker", {
-  once: function(obj, eventName, callback) {
-    var doCallback = function() {
-      callback.apply(obj, arguments);
-      obj.removeEventListener(eventName, doCallback);
-    };
-    obj.addEventListener(eventName, doCallback);
-  },
-  publish: {
-    locale: '',
-    showHeading: true,
-    narrow: {type: 'boolean', value: false, reflect: true},
-  },
-  observe: {
-    '$.calendarList._viewportSize': '_setCalendarReady',
-    '$.yearList._viewportSize': '_setYearListReady',
-  },
-  localeChanged: function() {
-    moment.locale(this.locale);
-  },
-  dateClicked: function(e) {
-    var element = e.originalTarget ? e.originalTarget : e.toElement;
-  },
-  ready: function() {
-    this.days = [];
-    this.months = [];
-    this.years = [];
-    this.today = new Date();
-    this.today.setHours(0, 0, 0, 0);
-    this.narrow = false;
-    this.weekdays = moment.weekdays();
-    this.monthNames = moment.months();
-    this.isTouch = 'ontouchstart' in window;
-    this.localeChanged();
-    this.startYear = 1900;
-    this.endYear = 2100;
-    this.populateCalendar();
-    this.headingDate = this.value ? this.value : new Date();
-    this.scrollToDate(this.headingDate);
+(function() {
+  var CALENDAR = 0
+  var YEARLIST = 1
 
-    // track page transitions
-    this.$.pages.addEventListener('core-animated-pages-transition-prepare', function() {
-      this._pageTransitioning = true;
-    });
-    this.$.pages.addEventListener('core-animated-pages-transition-end', function() {
-      this._pageTransitioning = false;
-    });
-  },
-  populateCalendar: function() {
-    var year, month, days, day, date = new Date();
-    var thisYear = this.today.getFullYear();
-    var thisMonth = this.today.getMonth();
-    var thisDay = this.today.getDate();
-
-    for (year=this.startYear; year<=this.endYear; year++) {
-      date.setYear(year);
-      for (month=0; month<12; month++) {
-        days = [];
-        day = 1;
-        date.setMonth(month);
-        date.setDate(1);
-        
-        // add "padding" days
-        for (d=0; d<date.getDay(); d++) {
-          days.push({day: null});
-        }
-
-        // add actual days 
-        while (date.getMonth() == month) {
-          days.push({
-            year: year,
-            month: month,
-            day: day,
-            isToday: year == thisYear && month == thisMonth && day == thisDay
-          });
-          date.setDate(++day);
-        }
-        monthData = {
-          year: year,
-          month: month,
-          days: days
-        };
-        this.months.push(monthData);
-        this.days.push(days);
+  Polymer("paper-date-picker", {
+    publish: {
+      locale: null,
+      minYear: null,
+      maxYear: null,
+      showHeading: true,
+      narrow: {type: 'boolean', value: false, reflect: true},
+      isTouch: {type: 'boolean', value: false, reflect: true}
+    },
+    dateClicked: function(e) {
+      var element = e.originalTarget ? e.originalTarget : e.toElement;
+    },
+    ready: function() {
+      this.today = this.$.calendar.today;
+      this.isTouch = 'ontouchstart' in window;
+      this.narrow = false;
+      this.localeChanged();
+      this.headingDate = this.value ? this.value : new Date();
+      this.years = this.$.calendar.years;
+    },
+    tapHeadingDay: function() {
+      this.$.calendar.scrollToDate(this.headingDate);
+      if (this.$.pages.selected !== CALENDAR) {
+        this.selectPage(CALENDAR);
       }
-      this.years.push({year: year});
-    }
-  },
-  getMonthIdx: function(year, month) {
-    var yearDiff = (year - this.months[0].year);
-    var monthDiff = (month - this.months[0].month);
-    return (yearDiff * 12) + monthDiff;
-  },
-  tapHeadingDay: function() {
-    if (this.$.pages.selected !== 0) {
-      this.selectPage(0, function() {
-        this.scrollToDate(this.headingDate);
-      }.bind(this));
-    } else {
-      this.scrollToDate(this.headingDate);
-    }
-  },
-  tapHeadingMonth: function() {
-    this.tapHeadingDay();
-  },
-  tapHeadingYear: function() {
-    var year = this.headingDate.getFullYear();
-    this.scrollYearList(year);
-    this.selectPage(1);
-  },
-  selectPage: function(page, callback) {
-    if (this._pageTransitioning) return;
-    if (callback) {
-      this.once(this.$.pages, 'core-animated-pages-transition-end', callback);
-    }
-    this.$.pages.selected = page;
-  },
-  scrollToMonth: function(year, month) {
-    var idx = this.getMonthIdx(year, month);
-    var cal = this.$.calendarList;
-    if (!this._calendarReady) {
-      this.addEventListener('calendar-ready', function() {
-        cal.scrollToGroup(idx);
+    },
+    tapHeadingMonth: function() {
+      this.tapHeadingDay();
+    },
+    tapHeadingYear: function() {
+      var year = this.headingDate.getFullYear();
+      this.scrollToYear(year);
+      if (this.$.pages.selected !== YEARLIST) {
+        this.$.yearSelector.selected = year.toString();
+        this.selectPage(YEARLIST);
+      }
+    },
+    selectPage: function(page, callback) {
+      this.$.pages.selected = page;
+    },
+    scrollToYear: function(year) {
+      this.async(function() {
+        var el = this.$.yearList.querySelector('.year[label="' + year + '"]')
+        this.$.yearList.scrollTop = el.offsetTop - 94;
       });
-    } else {
-      cal.scrollToGroup(idx);
-    }
-  },
-  scrollToDate: function(date) {
-    if (!date) {
-      date = this.value;
-    }
-    this.scrollToMonth(date.getFullYear(), date.getMonth());
-  },
-  scrollYearList: function(year, cb) {
-    var idx = year - this.startYear;
-    if (!this._yearListReady) {
-      this.addEventListener('year-list-ready', function() {
-        this.$.yearList.scrollToItem(idx);
-        this.$.yearList.scrollTop -= 94;
-        if(cb) cb();
-      }.bind(this));
-    } else {
-      this.$.yearList.scrollToItem(idx);
-      this.$.yearList.scrollTop -= 94;
-      if(cb) cb();
-    }
-  },
-  _setCalendarReady: function() {
-    if (!this._calendarReady) {
-      this._calendarReady = true;
-      this.fire('calendar-ready');
-    }
-  },
-  _setYearListReady: function() {
-    if (!this._yearListReady) {
-      this._yearListReady = true;
-      this.fire('year-list-ready');
-    }
-  },
-  selectedDateChanged: function() {
-    console.log(arguments);
-    var d = this.selectedDate;
-    this.value = new Date(d.year, d.month, d.day);
-  },
-  selectedYearChanged: function(oldValue, newValue) {
-    console.log('year changed');
-    this.scrollToMonth(this.selectedYear.year, this.headingDate.getMonth());
-    if (!oldValue) return;
-    if (this.$.pages.selected == 1) {
-      this.selectPage(0);
-    }
-  },
-  headingDateChanged: function() {
-    if (!this.selectedYear) {
-      var idx = this.headingDate.getFullYear() - this.startYear;
-      if (!this._yearListReady) {
-        this.addEventListener('year-list-ready', function() {
-          this.$.yearList.selectItem(idx);
+    },
+    yearSelected: function(e, detail) {
+      var year = parseInt(detail.item.getAttribute('label'));
+      var headingDate = new Date(this.headingDate)
+      if (year !== headingDate.getFullYear()) {
+        this.async(function() {
+          headingDate.setFullYear(year);
+          this.headingDate = headingDate;
         });
-      } else {
-        this.$.yearList.selectItem(idx);
+        this.$.calendar.populate(year);
+        this.async(function() {
+          this.$.calendar.scrollToDate(this.headingDate);
+          this.selectPage(CALENDAR)
+        });
       }
-    }
-  },
-  valueChanged: function() {
-    console.log('valueChanged: ', this.value);
-    this.headingDate = this.value;
-    var year = this.value.getFullYear();
-    var month = this.value.getMonth();
-    var idx = this.getMonthIdx(year, month);
-    var monthStart = (new Date(year, month, 1)).getDay();
-    var day = this.days[idx][monthStart + this.value.getDate() - 1];
-    if (!this._calendarReady) {
-      this.addEventListener('calendar-ready', function() {
-        this.$.calendarList.$.selection.select(day);
-      });
-    } else {
-      this.$.calendarList.$.selection.select(day);
-    }
-  },
-});
-
-PolymerExpressions.prototype.dateFormat = function(date, format) {
-  if (!date) return '';
-  return moment(date).format(format);
-};
+    },
+    dateSelected: function(e, detail) {
+      this.value = detail.date;
+    },
+    valueChanged: function(oldValue, newValue) {
+      if (newValue) {
+        this.headingDate = newValue;
+      } else {
+        this.headingDate = this.today;
+      }
+    },
+    // attributes that propagate to paper-calendar
+    localeChanged: function() {
+      moment.locale(this.locale);
+      this.$.calendar.locale = this.locale;
+    },
+    minYearChanged: function() {
+      this.$.calendar.minYear = this.minYear;
+    },
+    maxYearChanged: function() {
+      this.$.calendar.maxYear = this.maxYear;
+    },
+  });
+})();

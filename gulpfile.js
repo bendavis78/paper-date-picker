1f388e730787ab2be32807dacf8c635e1b478435
Ben Davis <bendavis78@gmail.com>
2015-11-07 19:03:02 -0600

    Moved CSS into dom modules

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var bump = require('gulp-bump');
 var tagVersion = require('gulp-tag-version');
 var git = require('gulp-git');
 var filter = require('gulp-filter');
+var portfinder = require('portfinder');
 
 function getPackage() {
   return JSON.parse(fs.readFileSync('bower.json', 'utf8')); 
@@ -90,11 +91,17 @@ gulp.task('serve', ['bower'], function () {
     }
   };
   opts.server.routes[pkgRoot] = '.';
-  browserSync.init(opts);
+
+  portfinder.basePort = 3000;
+  portfinder.getPort(function (err, port) {
+    opts.port = port;
+    console.log('using port: ' + port);
+    browserSync.init(opts);
+  });
 
   gulp.watch(['**/*.html']).on('change', reload);
   gulp.watch(['**/*.css']).on('change', reload);
-  gulp.watch(['bower.json']).on('change', 'bower:reload');
+  //gulp.watch(['bower.json']).on('change', 'bower:reload');
 });
 
 gulp.task('gh-pages', function() {
